42fe898480dee2ca6bdd133f9bb64d589074ad40
bonhoeffer-host <host@bonhoeffermachines.com>
2025-07-08 13:39:58 +0530

    Update about.js

--- components/sections/about.js
+++ components/sections/about.js
@@ -5,10 +5,8 @@ function About() {
     <section className="w-full py-16 bg-white">
       <div className="container mx-auto px-4 flex flex-col-reverse md:flex-row items-center gap-12">
         <div className="w-full md:w-1/2">
-          <h2 className="text-4xl md:text-5xl font-bold mb-6 text-gray-900">Why</h2>
-          <p className="text-gray-700 text-lg mb-4">
-            At <span className="font-semibold text-blue-700">Mechnova ?</span>, we are passionate about powering progress in agriculture, gardening, and industry through reliable, innovative, and affordable machinery. Backed by years of experience in international trade and engineering excellence, we bring to market a diverse portfolio of machines including brush cutters, chainsaws, sprayers, engines, generators, pumps, and more—designed to meet the evolving needs of modern users.
-          </p>
+          <h2 className="text-4xl md:text-5xl font-bold mb-6 text-gray-900">Why <span className="font-semibold text-blue-700">Mechnova ?</span></h2>
+         
           <p className="text-gray-700 text-lg mb-4">
            Because not everyone needs premium machines.
           </p>
