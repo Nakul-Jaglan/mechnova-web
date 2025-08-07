6cd0007c32c5b6a9e21a66a86add71a2dec77edc
Bonhoeffer Machines <host@bonhoeffermachines.com>
2025-08-07 15:29:48 +0530

    Update house.js

--- components/sections/house.js
+++ components/sections/house.js
@@ -11,7 +11,7 @@ function House() {
       name: "Bonhoeffer Machines",
       description: t('home.house.parentCompany'),
       url: "https://bonhoeffermachines.com",
-      logo: "/logos/bon_letter.png"
+      logo: "/logos/bonhoeffer_logo.png"
     },
     {
       name: "Stronwell",
